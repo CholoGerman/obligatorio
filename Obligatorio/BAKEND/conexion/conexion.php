5b2a40f378399b8cf2615dd463acbb71f8944d17
CholoGerman <xrcholo@gmail.com>
2024-04-25 16:40:10 -0300

    jioij

--- Obligatorio/BAKEND/conexion/conexion.php
+++ Obligatorio/BAKEND/conexion/conexion.php
@@ -1,1 +1,11 @@
-ehjyu
+<?php
+function connection()
+{
+    $host = "localhost";
+    $bd = "nombre de la base de datos";
+    $usuario = "root";
+    $password = "";
+    $puerto = "3306";
+    $mysqli = new mysqli($host, $usuario, $password, $bd, $puerto);
+    return $mysqli;
+}

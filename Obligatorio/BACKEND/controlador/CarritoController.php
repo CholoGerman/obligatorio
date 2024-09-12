c8f32c761d08b47b470a4283ecdacd1de01ef757
CholoGerman <xrcholo@gmail.com>
2024-09-12 15:06:14 -0300

    kkkk

--- Obligatorio/BACKEND/controlador/CarritoController.php
+++ Obligatorio/BACKEND/controlador/CarritoController.php
@@ -8,7 +8,17 @@ switch ($funcion) {
 }
 
 function realizarCompra(){
-  
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $departamento = $_POST["departamento"];
+    $ciudad = $_POST["ciudad"];
+    $calle = $_POST["calle"];
+    $numero = $_POST["numero"];
+    $telefono = $_POST["telefono"];
+  //dudas  $id_repuesto = $_POST["id_repuesto"];
+
+    $resultado = (new CarritoDao())->realizarCompra($nombre, $apellido, $departamento, $ciudad, $calle, $numero, $telefono);
+    echo json_encode($resultado);
 
 
 }
--- Obligatorio/BACKEND/modelo/CarritoDAO.php
+++ Obligatorio/BACKEND/modelo/CarritoDAO.php
@@ -4,8 +4,14 @@ require_once "../conexion/conexion.php";
 class CarritoDao{
 
 function realizarCompra(){
-
-
+    $sql = ""; // agregar un pedido?
+    $sql = ""; // borrar la publicacion?
+    $sql = ""; // actualizar stock?
+    $sql = ""; // crear la factura?
+    $connection = connection();
+    $respuesta = $connection->query($sql);
+    return $respuesta;
+    return new Respuesta(true, "Agregado correctamente", null);
 }
 
 

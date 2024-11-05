4eae95f3652ee7cb8e60f74776f10d8c9249b659
MariaAmbroggio <macelnetto@gmail.com>
2024-11-05 17:13:26 -0300

    Maria

--- frontend/PAGE/favoritos/favoritos.js
+++ frontend/PAGE/favoritos/favoritos.js
@@ -13,10 +13,10 @@ function mostrarFavoritos(favoritos) {
         console.log("No se han recibido productos.");
     }
     let tbodyElement = document.querySelector("#contenedor_producto");
-    tbodyElement.innerHTML = "";  
+    tbodyElement.innerHTML = "";
 
     favoritos.forEach((favorito) => {
-        console.log(favorito); 
+        console.log(favorito);
         tbodyElement.innerHTML += ` 
         <div class="contenedor_producto2" data-nombre="${favorito.nombre.toLowerCase()}">                                                      
             <a href="../producto/detalle_Producto.html?id_repuesto=${favorito.id_repuesto}">
@@ -28,6 +28,6 @@ function mostrarFavoritos(favoritos) {
             </div>
         </div>
     `;
-    
+
     });
 }

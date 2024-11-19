9fe718454b9ae82ea9712f8a891e82feb9ea00b3
CholoGerman <xrcholo@gmail.com>
2024-11-19 17:36:57 -0300

    origin

--- frontend/DAO/pedidoDAO.js
+++ frontend/DAO/pedidoDAO.js
@@ -1,3 +1,5 @@
+import origen from "../config/origin.js";
+
 window.onload =()=> {
       
     mostrarPedido(pedido);
@@ -53,8 +55,7 @@ async obtenerPedidosCliente(id_cliente) {
 
 
 async cambiarEstadoPedido(id_pedido, estado) {
-    $origen = getOrigin();
-    let url = $origen + "/backend/controlador/PedidosController.php?funcion=estado";
+    let url = origen + "/backend/controlador/PedidosController.php?funcion=estado";
     let formData = new FormData();
     formData.append("estado", estado);  
     formData.append("id_pedido", id_pedido);  
